6a3429bad165acd6ba63e851f6646d812680e2c5
Mintesinot getu <148215622+mntc3434@users.noreply.github.com>
2025-08-14 22:43:31 +0300

    Update qrcodegenerator.py

--- qrcodegenerator/qrcodegenerator.py
+++ qrcodegenerator/qrcodegenerator.py
@@ -4,7 +4,7 @@ qr = qrcode.QRCode(
     version= 10,
     box_size = 5,
     border=4
-)...
+)..
 data = "https://www.google.com/maps/@8.9892901,38.7576607,11.72z?entry=ttu"
 qr.add_data(data)
 qr.make(fit=True)
